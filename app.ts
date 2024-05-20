842ace1cf353fe11e41c5bfafbbe9d838b7e0a77
Paul K <spkim0921@gmail.com>
2024-05-20 10:56:39 -0700

    update

--- app.ts
+++ app.ts
@@ -8,7 +8,7 @@ import addresses from "./routes/addresses"
 
 dotenv.config()
 const app = express()
-const port = process.env.PORT
+const port = process.env.PORT || 3000
 
 app.use(cors())
 app.use(express.json())
